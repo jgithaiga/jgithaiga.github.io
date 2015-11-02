5c3fc3933170313a5326a1b1c6645bcfcac4ff3f
Julius Githaiga <juliusgthg@gmail.com>
2015-11-02 19:56:15 +0300

    Optimize required files

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -27,10 +27,10 @@ var cbpAnimatedHeader = (function() {
 	function scrollPage() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
+			jQuery(header).addClass( 'navbar-shrink' );
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
+			jQuery(header).removeClass( 'navbar-shrink' );
 		}
 		didScroll = false;
 	}
